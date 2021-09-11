f2f3f544d8a6ca8592989f6f3c111037561ebffb
Samayun Miah Chowdhury <samayun.m.chowdhury@gmail.com>
2021-09-11 19:54:18 +0600

    Dynamic database connection for mongodb improved

--- database/mysql.js
+++ database/mysql.js
@@ -1,10 +1,9 @@
-
 module.exports = async function connectDB() {
-    try {
-        // connection codes
-        return Promise.resolve(`Database Connected`);
-    } catch (error) {
-        return Promise.reject(error.message);
-    }
+  try {
+    // connection codes
+    return Promise.resolve(`Database Connected`);
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
 };
 // make a reusable multiple database connectivity functionalities here
