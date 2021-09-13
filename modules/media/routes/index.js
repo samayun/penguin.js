3f3c0fbaa8b488afa710a44be02a90a79d76b415
Samayun Miah Chowdhury <samayun.m.chowdhury@gmail.com>
2021-09-13 20:03:10 +0600

    pause....

--- modules/media/routes/index.js
+++ modules/media/routes/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 // manual import
 const fs = require("fs");
+const path = require("path");
 const multer = require("multer");
+
+const { URL } = require("url");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
 const upload = multer({ dest: "uploads/" });
 
 module.exports = (routes) => {
@@ -65,10 +71,162 @@ module.exports = (routes) => {
     }
   );
 
-  router.get("/files/:file", (req, res) => {
-    // do a bunch of if statements to make sure the user is
-    // authorized to view this image, then
-    const readStream = fs.createReadStream(`uploads/${req.params.file}`);
-    readStream.pipe(res);
+  router.get("/files/:file", (req, res, next) => {
+    try {
+      // do a bunch of if statements to make sure the user is
+      // authorized to view this image, then
+      const readStream = fs.createReadStream(`uploads/${req.params.file}`);
+      readStream.pipe(res);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  let uploads = {};
+
+  router.post("/upload", (req, res, next) => {
+    let fileId = req.headers["x-file-id"];
+    let startByte = parseInt(req.headers["x-start-byte"], 10);
+    let name = req.headers["name"];
+    let fileSize = parseInt(req.headers["size"], 10);
+    res.setHeader("Content-Length", fileSize);
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    // res.setHeader("Transfer-Encoding", "chunked");
+
+    console.log("file Size", fileSize, fileId, startByte);
+    if (uploads[fileId] && fileSize == uploads[fileId].bytesReceived) {
+      res.end();
+      return;
+    }
+
+    console.log(fileSize);
+
+    if (!fileId) {
+      res.writeHead(400, "No file id");
+      res.end(400);
+    }
+    console.log(uploads[fileId]);
+    if (!uploads[fileId]) uploads[fileId] = {};
+
+    let upload = uploads[fileId];
+
+    let fileStream;
+
+    if (!startByte) {
+      upload.bytesReceived = 0;
+      let name = req.headers["name"];
+      fileStream = fs.createWriteStream(`./uploads/${name}`, {
+        flags: "w",
+      });
+    } else {
+      if (upload.bytesReceived != startByte) {
+        res.writeHead(400, "Wrong start byte");
+        res.end(upload.bytesReceived);
+        return;
+      }
+      // append to existing file
+      fileStream = fs.createWriteStream(`./uploads/${name}`, {
+        flags: "a",
+      });
+    }
+
+    req.on("data", function (data) {
+      //console.log("bytes received", upload.bytesReceived);
+      upload.bytesReceived += data.length;
+    });
+
+    req.pipe(fileStream);
+
+    // when the request is finished, and all its data is written
+    fileStream.on("close", function () {
+      console.log(upload.bytesReceived, fileSize);
+      if (upload.bytesReceived == fileSize) {
+        console.log("Upload finished");
+        delete uploads[fileId];
+
+        // can do something else with the uploaded file here
+        res.send({ status: "uploaded" });
+        res.end();
+      } else {
+        // connection lost, we leave the unfinished file around
+        console.log("File unfinished, stopped at " + upload.bytesReceived);
+        res.writeHead(
+          500,
+          "File unfinished, stopped at " + upload.bytesReceived
+        );
+        res.end();
+      }
+    });
+
+    // in case of I/O error - finish the request
+    fileStream.on("error", function (err) {
+      console.log("fileStream error", err);
+      res.writeHead(500, "File error");
+      res.end();
+    });
+  });
+
+  async function convertVideoToMultipleResolution(video) {
+    // let blob = await File(video)
+    const url = URL.createObjectURL(video);
+    console.log(url);
+
+    await exec(`ffmpeg -i ${url} -vcodec libx265 -crf 36 public/${video}`);
+  }
+
+  router.post("/convert", async (req, res, next) => {
+    if (!req.files) {
+      return res.status(500).send({ msg: "file is not found" });
+    }
+    try {
+      // console.log(req.files.video)
+      await convertVideoToMultipleResolution(req.files.video);
+      res.json({ success: true });
+    } catch (error) {
+      res.json({ message: error.message });
+    }
+  });
+
+  router.get("/status", (req, res) => {
+    let fileId = req.headers["x-file-id"];
+    let name = req.headers["name"];
+    let fileSize = parseInt(req.headers["size"], 10);
+    console.log(
+      Math.floor(fileSize / 1024) > 1000
+        ? Math.floor(fileSize / 1024) / 1000 + "MB"
+        : Math.floor(fileSize / 1024) + "KB"
+    );
+    // res.setHeader("Transfer-Encoding", "chunked");
+
+    if (name) {
+      try {
+        let stats = fs.statSync("uploads/" + name);
+        if (stats.isFile()) {
+          console.log(
+            `fileSize is ${
+              Math.floor(fileSize / 1024) > 1000
+                ? Math.floor(fileSize / 1024) / 1000 + "MB"
+                : Math.floor(fileSize / 1024) + "KB"
+            } and already uploaded file size ${stats.size}`
+          );
+          if (fileSize == stats.size) {
+            res.send({ status: "file is present", uploaded: stats.size });
+            return;
+          }
+          if (!uploads[fileId]) uploads[fileId] = {};
+          console.log(uploads[fileId]);
+          uploads[fileId]["bytesReceived"] = stats.size;
+          console.log(uploads[fileId], stats.size);
+        }
+      } catch (er) {}
+    }
+    let upload = uploads[fileId];
+
+    // res.setHeader("Content-Length",parseInt(req.headers["size"], 10));
+    // res.setHeader("Content-Type","application/json; charset=utf-8");
+
+    if (upload) res.status(200).send({ uploaded: upload.bytesReceived });
+    else res.send({ uploaded: 0 });
   });
 };
